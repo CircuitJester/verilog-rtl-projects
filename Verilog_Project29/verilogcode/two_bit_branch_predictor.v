e10ac9ebdea136c13af0430c9c90c40b02bfbc25
Omm Prakash Sahoo <sahooommprakash42@gmail.com>
2026-08-14 07:25:41 +0000

    Refine Project 29 branch predictor RTL

--- Verilog_Project29/verilogcode/two_bit_branch_predictor.v
+++ Verilog_Project29/verilogcode/two_bit_branch_predictor.v
@@ -14,47 +14,63 @@ localparam WEAKLY_NOT_TAKEN = 2'b01;
 localparam WEAKLY_TAKEN = 2'b10;
 localparam STRONGLY_TAKEN = 2'b11;
 
-
 assign predict_taken = prediction_state[1];
 
+always @(posedge clk) 
 
-always @(posedge clk) begin
-    if (rst) begin
-        prediction_state <= WEAKLY_NOT_TAKEN;
-    end
-    else if (update_enable) 
+begin
 
+    if (rst) 
     begin
+        prediction_state <= WEAKLY_NOT_TAKEN;
 
+    end
+
+
+    else if (update_enable) 
+    
+    begin
         case (prediction_state)
-            STRONGLY_NOT_TAKEN: begin
+            STRONGLY_NOT_TAKEN: 
+            
+            begin
                 if (branch_taken_actual)
                     prediction_state <= WEAKLY_NOT_TAKEN;
+
             end
 
-            WEAKLY_NOT_TAKEN: begin
+            WEAKLY_NOT_TAKEN: 
+            
+            begin
                 if (branch_taken_actual)
                     prediction_state <= WEAKLY_TAKEN;
                 else
                     prediction_state <= STRONGLY_NOT_TAKEN;
+
             end
 
-            WEAKLY_TAKEN: begin
+            WEAKLY_TAKEN: 
+            
+            begin
                 if (branch_taken_actual)
                     prediction_state <= STRONGLY_TAKEN;
                 else
                     prediction_state <= WEAKLY_NOT_TAKEN;
+
             end
 
-            STRONGLY_TAKEN: begin
+            STRONGLY_TAKEN: 
+            begin
                 if (!branch_taken_actual)
                     prediction_state <= WEAKLY_TAKEN;
+                    
             end
 
             default:
                 prediction_state <= WEAKLY_NOT_TAKEN;
 
         endcase
+
     end
 
 end
